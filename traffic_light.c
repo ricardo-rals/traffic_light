0df9342b1dfbde2cd9ae9b9e1d94586c87f2943e
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2025-02-04 10:56:30 -0300

    feat: implement main application logic

--- traffic_light.c
+++ traffic_light.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "./includes/traffic_lights.h"
+#include "./includes/leds.h"
 
-
-
-int main()
-{
+int main() {
+    // Inicializa a stdio para usar a porta serial
     stdio_init_all();
 
+    // Inicializa o semáforo
+    traffic_light_init();
+
+    // Inicializa o temporizador
+    timer_init();
+
+    // Loop principal
     while (true) {
-        printf("Hello, world!\n");
+        // Imprime o estado atual do semáforo a cada segundo
+        printf("Estado atual do semáforo: %d\n", traffic_light_get_state());
         sleep_ms(1000);
     }
+
+    return 0;
 }
